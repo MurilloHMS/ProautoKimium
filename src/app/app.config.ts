52e3ea86a57a5fd690c9f9ee709f8c83f2c07bdb
MurilloHMS <murillohenriquemelo@outlook.com>
2025-09-16 13:16:48 -0300

    refactor: streamline app configuration and update styles
    - Removed AuthInterceptor and AuthService from app configuration.
    - Simplified HTTP client provider setup.
    - Updated styles to include Bootstrap and removed PrimeNG styles.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,18 +4,11 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
-import { AuthInterceptor } from './Core/interceptors/auth-interceptor';
-import { AuthService } from './Core/services/auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-     provideHttpClient(withInterceptorsFromDi()),
-     {
-      provide : HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-     }
+    provideHttpClient(withInterceptorsFromDi()),
   ]
 };
