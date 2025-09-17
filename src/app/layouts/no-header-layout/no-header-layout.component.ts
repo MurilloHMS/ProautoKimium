e14ddd4028750988fa07fb2b68fc5ae642b1c7b7
MurilloHMS <murillohenriquemelo@outlook.com>
2025-09-17 14:19:05 -0300

    feat: add NoHeaderLayoutComponent with template, styles, and tests

---
+++ src/app/layouts/no-header-layout/no-header-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-no-header-layout',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './no-header-layout.component.html',
+  styleUrl: './no-header-layout.component.scss'
+})
+export class NoHeaderLayoutComponent {
+
+}
