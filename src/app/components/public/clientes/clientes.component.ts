59f3ae081c0537469fe9c5ef3deaf70bce609acb
MurilloHMS <murillohenriquemelo@outlook.com>
2025-09-17 16:38:32 -0300

    refactor: remove unused CardCaseSucessoComponent import from ClientesComponent

--- src/app/components/public/clientes/clientes.component.ts
+++ src/app/components/public/clientes/clientes.component.ts
@@ -1,11 +1,10 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CardCaseSucessoComponent } from "../../Components/card-case-sucesso/card-case-sucesso.component";
 
 @Component({
   selector: 'app-clientes',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [CardCaseSucessoComponent],
+  imports: [],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.scss'
 })
