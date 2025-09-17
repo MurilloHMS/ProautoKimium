44be3bbd8d5125e0731b177b014bc451c557c425
MurilloHMS <murillohenriquemelo@outlook.com>
2025-09-17 10:30:16 -0300

    feat: add HoleritSpliterComponent with template, styles, and tests

---
+++ src/app/components/auth/documents/holerit-spliter/holerit-spliter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-holerit-spliter',
+  standalone: true,
+  imports: [],
+  templateUrl: './holerit-spliter.component.html',
+  styleUrl: './holerit-spliter.component.scss'
+})
+export class HoleritSpliterComponent {
+
+}
