7c82645a10092472fc5e2f46b729156a7cd46d97
MurilloHMS <murillohenriquemelo@outlook.com>
2025-09-17 16:38:16 -0300

    feat: add Customer and CustomerResponse interfaces for customer data modeling

---
+++ src/app/domain/models/customer.model.ts
@@ -0,0 +1,22 @@
+export interface Customer {
+  codParceiro: string;
+  documento: string;
+  name: string;
+  email: string;
+  ativo: boolean;
+  recebeEmail: boolean;
+  codMatriz: string;
+}
+
+export interface CustomerResponse {
+  id: string;
+  codParceiro: string;
+  documento: string;
+  name: string;
+  email: {
+    address: string;
+  }
+  ativo: boolean;
+  recebeEmail: boolean;
+  codMatriz: string;
+}
