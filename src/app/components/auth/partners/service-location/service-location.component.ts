fae1efaeec295b561e042bd95ca86ffdee62eb6f
MurilloHMS <murillohenriquemelo@outlook.com>
2025-09-17 10:30:58 -0300

    feat: add ServiceLocationComponent with template, styles, and tests

---
+++ src/app/components/auth/partners/service-location/service-location.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './service-location.component.html',
+  styleUrl: './service-location.component.scss'
+})
+export class ServiceLocationComponent {
+
+}
