68de5a55ddfcdbddc707637eced016f1a595a1b1
MurilloHMS <murillohenriquemelo@outlook.com>
2025-09-17 11:55:26 -0300

    refactor: improve formatting and readability of AuthInterceptor

--- src/app/Core/interceptors/auth-interceptor.ts
+++ src/app/Core/interceptors/auth-interceptor.ts
@@ -1,24 +1,25 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { AuthService } from "../services/auth.service";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({ providedIn: 'root' })
-export class AuthInterceptor implements HttpInterceptor{
-    constructor(private authService: AuthService){}
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const token = this.authService.getToken();
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authService.getToken();
+    console.log('Interceptor chamado, token:', token);
 
-        if(token){
-            const cloned = req.clone({
-                setHeaders: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            return next.handle(cloned);
+    if (token) {
+      const cloned = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
         }
-
-        return next.handle(req);
+      });
+      console.log('Request clonada com Authorization:', cloned.headers.get('Authorization'));
+      return next.handle(cloned);
     }
+    return next.handle(req);
+  }
 }
