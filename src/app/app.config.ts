29df588a6d6caedf7cca4895df076a74d4f2f06f
MurilloHMS <murillohenriquemelo@outlook.com>
2025-09-18 13:50:29 -0300

    fix: theme conflicts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,19 +2,45 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
+import { definePreset } from '@primeuix/themes';
+import Material from '@primeuix/themes/material';
 import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './infrastructure/interceptors/auth-interceptor';
 
+const MaterialEmerald = definePreset(Material, {
+  semantic: {
+    primary: {
+      50: '#e8f5e9', // Verde claro
+      100: '#c8e6c9',
+      200: '#a5d6a7',
+      300: '#81c784',
+      400: '#66bb6a',
+      500: '#4caf50', // Cor primária (verde esmeralda)
+      600: '#43a047',
+      700: '#388e3c',
+      800: '#2e7d32',
+      900: '#1b5e20',
+      950: '#0a3d0c',
+    },
+  },
+});
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     providePrimeNG({
+      ripple: true,
       theme: {
-        preset: Aura
+          preset: Aura,
+          options: {
+              prefix: 'p',
+              darkModeSelector: 'dark-mode',
+              cssLayer: false
+          }
       }
     }),
     provideHttpClient(withInterceptorsFromDi()),
