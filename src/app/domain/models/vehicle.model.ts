75ba619666ee5b116ad079a1f17109555bc97727
MurilloHMS <murillohenriquemelo@outlook.com>
2025-09-26 10:45:21 -0300

    feat: include vehicle model

---
+++ src/app/domain/models/vehicle.model.ts
@@ -0,0 +1,9 @@
+export interface Vehicle{
+    nome: string;
+    placa: string;
+    marca: string;
+    consumoUrbanoAlcool: number;
+    consumoUrbanoGasolina: number;
+    consumoRodoviarioAlcool: number;
+    consumoRodoviarioGasolina: number;
+}
