941c8cd5fb7f2b62c4fb085f9e66fadaea3fffb2
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-01 12:50:37 -0300

    feat: include generate token on admin center

--- src/app/components/auth/admin-center/admin-center.component.ts
+++ src/app/components/auth/admin-center/admin-center.component.ts
@@ -1,11 +1,29 @@
+import { AuthService } from './../../../infrastructure/services/auth.service';
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { Button } from "primeng/button";
+import { Toast } from 'primeng/toast';
 
 @Component({
     selector: 'app-admin-center',
-    imports: [],
+    imports: [Button, Toast],
     templateUrl: './admin-center.component.html',
-    styleUrl: './admin-center.component.scss'
+    styleUrl: './admin-center.component.scss',
+    providers: [MessageService]
 })
 export class AdminCenterComponent {
+  loading: boolean = false;
 
+  constructor(private authService: AuthService,
+              private messageService: MessageService
+  ) {}
+
+  generateAccessTokens() {
+    this.loading = true;
+    this.authService.getStockControlToken().subscribe(token => {
+      navigator.clipboard.writeText(token);
+      this.messageService.add({severity:'success', summary: 'Success', detail: 'Token copiado para o clipboard!'});
+      this.loading = false;
+    })
+  }
 }
