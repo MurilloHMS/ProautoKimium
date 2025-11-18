417224e115d42b8d2c9f98720c76263481531feb
MurilloHMS <murillohenriquemelo@outlook.com>
2025-11-18 17:04:47 -0300

    feat: include contact button on header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CartService } from '../../infrastructure/services/cart.service';
+import { DialogModule } from 'primeng/dialog';
+import { ContactComponent } from '../../shared/contact/contact.component';
 
 
 @Component({
     selector: 'app-header',
-    imports: [RouterLink],
+    imports: [RouterLink, DialogModule, ContactComponent],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
   quantidadeProdutos = 0;
   menuAberto = false;
+  visible = false;
 
   toggleMenu() {
     this.menuAberto = !this.menuAberto;
@@ -23,5 +26,9 @@ export class HeaderComponent implements OnInit {
       this.cartService.getCartCount().subscribe(count => {
         this.quantidadeProdutos = count;
       })
+  }
+
+  showContact() {
+    this.visible = true;
   }
 }
