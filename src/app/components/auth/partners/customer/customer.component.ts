a79c3fab3c986aa401dfca082eb2c74779da223b
MurilloHMS <murillohenriquemelo@outlook.com>
2025-12-01 11:05:39 -0300

    feat: include import customer by excel

--- src/app/components/auth/partners/customer/customer.component.ts
+++ src/app/components/auth/partners/customer/customer.component.ts
@@ -6,28 +6,35 @@ import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import { ToolbarModule } from 'primeng/toolbar';
-import { Customer, CustomerResponse } from '../../../../domain/models/customer.model';
+import { Customer } from '../../../../domain/models/customer.model';
 import { CustomerService } from '../../../../infrastructure/services/partners/customer/customer.service';
 import { CheckboxModule } from 'primeng/checkbox';
+import { FileUpload } from "primeng/fileupload";
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
     selector: 'app-customer',
-    imports: [TableModule, CommonModule, ButtonModule, ToolbarModule,
-        DialogModule, InputTextModule, ReactiveFormsModule, CheckboxModule
-    ],
+    imports: [TableModule, CommonModule, ButtonModule, ToolbarModule, ToastModule,
+    DialogModule, InputTextModule, ReactiveFormsModule, CheckboxModule, FileUpload],
     templateUrl: './customer.component.html',
-    styleUrl: './customer.component.scss'
+    styleUrl: './customer.component.scss',
+    providers: [MessageService]
 })
 export class CustomerComponent {
-  customers: CustomerResponse[] = [];
+  customers: Customer[] = [];
   loading: boolean = false;
   visible: boolean = false;
   customer: Customer | null = null;
   form: FormGroup;
   dialogTitle: string = 'Adicionar Cliente';
-  customerToEdit: CustomerResponse | null = null;
+  customerToEdit: Customer | null = null;
+  isUploading: boolean = false;
+  selectedFile: File | null = null;
 
-  constructor(private customerService: CustomerService, private fb: FormBuilder){
+  constructor(private customerService: CustomerService,
+    private messageService: MessageService,
+    private fb: FormBuilder){
     this.form = this.fb.group({
       codParceiro: ['', Validators.required],
       documento: ['', Validators.required],
@@ -53,18 +60,18 @@ export class CustomerComponent {
     });
   }
 
-  editCustomer(customer: CustomerResponse) {
+  editCustomer(customer: Customer) {
     this.dialogTitle = 'Editar Cliente';
     this.customerToEdit = customer;
 
     this.form.patchValue({
       codParceiro: customer.codParceiro,
       documento: customer.documento,
-      nome: customer.name,
-      email: customer.email.address,
+      nome: customer.nome,
+      email: customer.email,
       ativo: customer.ativo,
       recebeEmail: customer.recebeEmail,
-      codigoMatriz: customer.codMatriz ?? null
+      codMatriz: customer.codMatriz ?? null
     });
     this.visible = true;
   }
@@ -105,4 +112,38 @@ export class CustomerComponent {
       }
     };
   }
+
+  importByExcel() {
+    if (!this.selectedFile) return;
+
+    this.loading = true;
+    this.isUploading = true;
+
+    this.customerService.importCustomersByExcel(this.selectedFile).subscribe({
+      next: (msg: string) => {
+        this.loading = false;
+        this.isUploading = false;
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Sucesso',
+          detail: msg
+        });
+      },
+      error: (err) => {
+        this.loading = false;
+        this.isUploading = false;
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erro',
+          detail: err.error || err.message || 'Erro desconhecido'
+        });
+      }
+    });
+  }
+
+
+  onFileSelect(event: any) {
+    this.selectedFile = event.files[0];
+  }
+
 }
