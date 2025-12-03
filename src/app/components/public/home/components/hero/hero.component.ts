ebb92cf24e82c85d3869b1cfae2b8bfbc11bf351
MurilloHMS <murillohenriquemelo@outlook.com>
2025-12-03 15:58:07 -0300

    feat: update hero section with new content and styles; enhance header layout

--- src/app/components/public/home/components/hero/hero.component.ts
+++ src/app/components/public/home/components/hero/hero.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { DialogModule } from 'primeng/dialog';
+import { ContactComponent } from '../../../../../shared/contact/contact.component';
 
 @Component({
     selector: 'app-hero',
-    imports: [],
+    imports: [RouterLink, DialogModule, ContactComponent],
     templateUrl: './hero.component.html',
     styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
+    menuAberto = false;
+    visible = false;
 
+    showContact() {
+    this.visible = true;
+  }
 }
