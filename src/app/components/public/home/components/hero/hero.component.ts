9300000ebe12745dce6463e6accb74f521f3a13a
MurilloHMS <murillohenriquemelo@outlook.com>
2026-02-10 07:44:57 -0300

    refactor: remove unused RouterLink import from hero component

--- src/app/components/public/home/components/hero/hero.component.ts
+++ src/app/components/public/home/components/hero/hero.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { DialogModule } from 'primeng/dialog';
 import { ContactComponent } from '../../../../../shared/contact/contact.component';
 
 @Component({
     selector: 'app-hero',
-    imports: [RouterLink, DialogModule, ContactComponent],
+    imports: [DialogModule, ContactComponent],
     templateUrl: './hero.component.html',
     styleUrl: './hero.component.scss'
 })
