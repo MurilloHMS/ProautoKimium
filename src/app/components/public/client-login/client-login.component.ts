315145a1e98831cfdad5059f9153ffed3b3ee87c
MurilloHMS <murillohenriquemelo@outlook.com>
2026-02-23 10:53:22 -0300

    layout: enhance client login UI and functionality

--- src/app/components/public/client-login/client-login.component.ts
+++ src/app/components/public/client-login/client-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-client-login',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './client-login.component.scss'
 })
 export class ClientLoginComponent {
+form: FormGroup;
+  errorMessage: string = '';
+  loading: boolean = false;
 
+  constructor(
+    private fb: FormBuilder,
+  ){
+    this.form = this.fb.group({
+      username: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9.]+$')]],
+      password: ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')]]
+    });
+  }
+
+  login(){
+
+  }
 }
