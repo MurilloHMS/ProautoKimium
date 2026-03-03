ee9c420f5205a2eee4a2e3c34634ce9faf6d0d7f
MurilloHMS <murillohenriquemelo@outlook.com>
2026-03-03 17:09:19 -0300

    fix: remove background color and duvidas component

--- src/app/components/public/home/home.component.ts
+++ src/app/components/public/home/home.component.ts
@@ -3,14 +3,13 @@ import { HeroComponent } from "./components/hero/hero.component";
 import { ProcessoConsultoriaComponent } from "./components/processo-consultoria/processo-consultoria.component";
 import { MotivosContratarComponent } from "./components/motivos-contratar/motivos-contratar.component";
 import { ServicosOfertadosComponent } from "./components/servicos-ofertados/servicos-ofertados.component";
-import { DuvidasComponent } from "./components/duvidas/duvidas.component";
 import { AboutUsComponent } from "./components/about-us/about-us.component";
 import { ToolbarComponent } from "./components/toolbar/toolbar.component";
 import {ContactComponent} from "../../../shared/contact/contact.component";
 
 @Component({
     selector: 'app-home',
-  imports: [HeroComponent, ProcessoConsultoriaComponent, MotivosContratarComponent, ServicosOfertadosComponent, DuvidasComponent, AboutUsComponent, ToolbarComponent, ContactComponent],
+  imports: [HeroComponent, ProcessoConsultoriaComponent, MotivosContratarComponent, ServicosOfertadosComponent, AboutUsComponent, ToolbarComponent, ContactComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
