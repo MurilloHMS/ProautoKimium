1df218cc9efd88e043dd0da0557502d5f1ec773d
MurilloHMS <murillohenriquemelo@outlook.com>
2026-03-10 10:34:53 -0300

    feat: create model

---
+++ src/app/domain/models/holerit.models.ts
@@ -0,0 +1,15 @@
+export interface PdfPageInfoExtractorDTO {
+  nome: string;
+  cargo: string;
+  cpf: string;
+  empresa: string;
+  departamento: string;
+  inss: number;
+  irrf: number;
+  inssFerias: number;
+  irrfFerias: number;
+  inss13: number;
+  irrf13: number;
+  fgts: number;
+  emprestimoTrabalhador: number;
+}
