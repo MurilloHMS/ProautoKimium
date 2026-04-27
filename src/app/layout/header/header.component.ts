7fd3e0ceb5a81c71abf10a217ea26ba06a590fcc
MurilloHMS <murillohenriquemelo@outlook.com>
2026-04-27 16:23:33 -0300

    style: melhorado o layout do header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,34 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { CartService } from '../../infrastructure/services/cart.service';
 import { DialogModule } from 'primeng/dialog';
 import { ContactComponent } from '../../shared/contact/contact.component';
 
-
 @Component({
-    selector: 'app-header',
-    imports: [RouterLink, DialogModule, ContactComponent],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.scss'
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink, CommonModule, DialogModule, ContactComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
   quantidadeProdutos = 0;
   menuAberto = false;
   visible = false;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cartService.getCartCount().subscribe(count => {
+      this.quantidadeProdutos = count;
+    });
+  }
+
   toggleMenu() {
     this.menuAberto = !this.menuAberto;
   }
 
-  constructor(private cartService: CartService) {}
-
-  ngOnInit() {
-      this.cartService.getCartCount().subscribe(count => {
-        this.quantidadeProdutos = count;
-      })
-  }
-
-  showContact() {
-    this.visible = true;
+  fecharMenuMobile() {
+    this.menuAberto = false;
   }
 }
