7be09c764668985205ce55ce204667f6505509aa
MurilloHMS <murillohenriquemelo@outlook.com>
2026-05-07 10:12:55 -0300

    fix: corrigido avisos

--- src/app/components/public/home/home.component.ts
+++ src/app/components/public/home/home.component.ts
@@ -4,13 +4,12 @@ import { ProcessoConsultoriaComponent } from "./components/processo-consultoria/
 import { MotivosContratarComponent } from "./components/motivos-contratar/motivos-contratar.component";
 import { ServicosOfertadosComponent } from "./components/servicos-ofertados/servicos-ofertados.component";
 import { AboutUsComponent } from "./components/about-us/about-us.component";
-import { ToolbarComponent } from "./components/toolbar/toolbar.component";
 import {ContactComponent} from "../../../shared/contact/contact.component";
 import {FraudAlertComponent} from "../alerts/fraud-alert/fraud-alert.component";
 
 @Component({
     selector: 'app-home',
-  imports: [HeroComponent, ProcessoConsultoriaComponent, MotivosContratarComponent, ServicosOfertadosComponent, AboutUsComponent, ToolbarComponent, ContactComponent, FraudAlertComponent],
+  imports: [HeroComponent, ProcessoConsultoriaComponent, MotivosContratarComponent, ServicosOfertadosComponent, AboutUsComponent, ContactComponent, FraudAlertComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
